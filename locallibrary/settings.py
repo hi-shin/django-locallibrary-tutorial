429b70e6c6f9708f81ad5e308d189af4b8330b76
Shin HBLAB <74754363+hi-shin@users.noreply.github.com>
2021-07-24 00:59:55 +0700

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -148,3 +148,6 @@ STATIC_URL = '/static/'
 # Static file serving.
 # http://whitenoise.evans.io/en/stable/django.html#django-middleware
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import django_heroku
+django_heroku.settings(locals())
